66bcdfd8d71dbd679071d0b16e3937553e312bc8
nuwanchathurangaa <165279317+nuwanchathurangaa@users.noreply.github.com>
2024-04-02 15:07:10 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,31 +3,31 @@ const fs = require('fs-extra')
   
   
  //═══════[Required Variables]════════\\ 
- global.owner = process.env.OWNER_NUMBER.split(",") 
+ global.owner = process.env.OWNER_NUMBER.split(",94766859177") 
  global.mongodb = process.env.MONGODB_URI || "mongodb+srv://sam:sam@cluster0.u1smxsv.mongodb.net/?retryWrites=true&w=majority" 
  global.port= process.env.PORT || 5000 
  global.email = 'sam@secktor.live' 
  global.github = 'https://github.com/SamPandey001/Secktor-Md' 
  global.location = 'Sultanpur IN' 
  global.gurl = 'https://instagram.com/' // add your username 
- global.sudo = process.env.SUDO || '919628516236' 
- global.devs = '919628516236'; 
- global.website = 'https://github.com/SamPandey001/Secktor-Md' //wa.me/+91000000000000 
+ global.sudo = process.env.SUDO || '94766859177' 
+ global.devs = '94766859177'; 
+ global.website = 'https://github.com/SamPandey001/Secktor-Md' //wa.me/+94766859177
  global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://raw.githubusercontent.com/SecktorBot/Brandimages/main/logos/SocialLogo%201.png' 
  module.exports = { 
-   botname:   process.env.BOT_NAME === undefined ? '𝐒𝐞𝐜𝐤𝐭𝐨𝐫 𝐁𝐨𝐭𝐭𝐨' : process.env.BOT_NAME, 
-   ownername: process.env.OWNER_NAME === undefined ? 'SamPandey001' : process.env.OWNER_NAME,  
+   botname:   process.env.BOT_NAME === undefined ? 'nuwa' : process.env.BOT_NAME, 
+   ownername: process.env.OWNER_NAME === undefined ? 'nuwa' : process.env.OWNER_NAME,  
    author:  process.env.PACK_INFO.split(";")[0] === undefined ? 'SamPandey001' : process.env.PACK_INFO.split(";")[0], 
-   auto_read_status :  process.env.AUTO_READ_STATUS === undefined ? false : process.env.AUTO_READ_STATUS, 
+   auto_read_status :  process.env.AUTO_READ_STATUS === undefined ? true : process.env.AUTO_READ_STATUS, 
    packname:  process.env.PACK_INFO.split(";")[1] === undefined ? 'Secktor-Md' : process.env.PACK_INFO.split(";")[1],
    sessionName:  process.env.SESSION_ID === undefined ? false : process.env.SESSION_ID, 
    autoreaction:  process.env.AUTO_REACTION  === undefined ? false : process.env.AUTO_REACTION , 
    antibadword :  process.env.ANTI_BAD_WORD === undefined ? 'nbwoed' : process.env.ANTI_BAD_WORD, 
-   alwaysonline:  process.env.ALWAYS_ONLINE === undefined ? false : process.env.ALWAYS_ONLINE,
+   alwaysonline:  process.env.ALWAYS_ONLINE === undefined ? true : process.env.ALWAYS_ONLINE,
    voice  :  process.env.AUTO_VOICE === undefined ? false : process.env.AUTO_VOICE, 
-   antifake : process.env.FAKE_COUNTRY_CODE === undefined ? '971' : process.env.FAKE_COUNTRY_CODE, 
+   antifake : process.env.FAKE_COUNTRY_CODE === undefined ? '94' : process.env.FAKE_COUNTRY_CODE, 
    readmessage:  process.env.READ_MESSAGE === undefined ? false : process.env.READ_MESSAGE, 
-   auto_status_saver: process.env.AUTO_STATUS_SAVER === undefined ? false : process.env.AUTO_STATUS_SAVER, 
+   auto_status_saver: process.env.AUTO_STATUS_SAVER === undefined ? true : process.env.AUTO_STATUS_SAVER, 
    HANDLERS:  process.env.PREFIX === undefined ? ['.'] : process.env.PREFIX, 
    warncount : process.env.WARN_COUNT === undefined ? 3 : process.env.WARN_COUNT, 
    disablepm:  process.env.DISABLE_PM === undefined ? false : process.env.DISABLE_PM, 
